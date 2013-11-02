89a74a6bee0ba9ae8f8c4231a19e4bec087fd804
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-11-01 22:01:17 -0700

    Adjust example

--- test/example.rb
+++ test/example.rb
@@ -1,10 +1,17 @@
 require 'menuboy'
 
-Menuboy.menu('main menu') do |m|
-  m.opt("start the stack") do
+Menuboy.menu('main menu') do |menu|
+  menu.option("start the stack") do
     puts "hi"
   end
-  m.opt("drop a database") do
-    puts "bye"
+  menu.option("drop a database") do |dropper|
+    dropper.menu("which database?") do |db|
+      db.option("this one") do 
+        puts "sounds good"
+      end
+      db.option("that one") do
+        puts "ok"
+      end
+    end
   end
 end
